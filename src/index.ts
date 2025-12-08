1c8914fa7b09717792701ab03c04ebb6c077e347
sakatimuna7 <sakatimunapro7@gmail.com>
2025-12-08 12:45:20 +0700

    feat: * membuat service icare fktp * membuat service icare fkrtl

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 export * from "./services/httpClient";
 export * from "./core/configHelper";
 export * from "./services/pcare/pcare.service";
+export * from "./services/antrean/fktpBPJS.service";
+export * from "./services/i-care/icare.service";
 export * from "./lib/redis";
+//
 export * from "./types/global";
 export * from "./types/pcare";
 export * from "./types/kunjungan";
@@ -9,3 +12,5 @@ export * from "./types/peserta";
 export * from "./types/kunjungan";
 export * from "./types/pendaftaran";
 export * from "./types/spesialis";
+export * from "./types/antreanFktp";
+export * from "./types/icare";
